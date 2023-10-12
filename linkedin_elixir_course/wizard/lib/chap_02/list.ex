2d102036d6d90feca83244507f38ce421eb49186
DelmiroDaladier <daladiersampaio@gmail.com>
2023-10-12 20:00:41 +0100

    add: tuple and list modules

---
+++ linkedin_elixir_course/wizard/lib/chap_02/list.ex
@@ -0,0 +1,27 @@
+defmodule Wizard.List do
+  @moduledoc """
+  This module contains functions for learning
+  about the List data type.
+  """
+
+  def check(value) do
+    is_list(value)
+  end
+
+  def add(list, value) do
+    IO.inspect([value|list])
+    IO.inspect(list ++ [value])
+    :ok
+  end
+
+  def demo do
+    a = ["toad", 2, {:ok, "green"}]
+
+    IO.inspect(a, label: "List")
+    IO.puts("First element: #{List.first(a)}")
+    IO.inspect(hd(a), label: "List Head")
+    IO.inspect(tl(a), label: "List Tail")
+    IO.inspect(length(a), label: "List Length")
+  end
+
+end
